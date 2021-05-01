214b1cd7a1f226b7fd9e73bbc2062f9412ef6147
Thomas Gassmann <thomas.gassmann@hotmail.com>
2021-05-01 12:45:00 +0200

    Add basic infrastructure for notifier
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

---
+++ pkg/romototo/notifications/notifier.go
@@ -0,0 +1,14 @@
+package notifications
+
+import "github.com/thomasgassmann/robomoto/pkg/romototo"
+
+type Notifier struct {
+}
+
+func (t *Notifier) Init() {
+		
+}
+
+func (t *Notifier) Send(housing []romototo.Housing) {
+
+}
